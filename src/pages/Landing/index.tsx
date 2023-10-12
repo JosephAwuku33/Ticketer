c3c9070fa9a911b59c6a23c1ebb1a592a4cb018e
JosephAwuku33 <33jaykay@gmail.com>
2023-10-12 04:12:49 +0000

    removed redundant code

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, Image, ScrollView } from "react-native";
+import { View } from "react-native";
 import EventList from "../../components/DataList/EventList";
 import LandingPageAbove from "../../components/LandingPageStuff/LandingPageAbove";
 
